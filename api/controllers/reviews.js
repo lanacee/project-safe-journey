3969893a1d31420d5d0153ea4c25ab79cb35ec5d
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-08-01 13:50:08 +1000

    add review controller for index route

---
+++ api/controllers/reviews.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const reviewsRouter = express.Router();
+const Review = require("../models/review.js");
+
+// index
+reviewsRouter.get("/", async (req, res) => {
+  const reviews = await Review.find({}).exec();
+  res.status(200).json(reviews);
+});
