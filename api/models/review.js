b7bf72014773f28c6f649254e7257bb47c065c17
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-08-02 19:54:04 +1000

    remove unique user id from review, pseudo code for review

--- api/models/review.js
+++ api/models/review.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
-    user_id: { type: String, unique: true },
-    country: String,
-    racism_experience: { type: Number, min: 0, max: 5},
-    lgbtqi_acceptance: { type: Number, min: 0, max: 5},
-    womens_safety: { type: Number, min: 0, max: 5},
-    description: String,
-}
-)
+  user_id: { type: String },
+  country: String,
+  racism_experience: { type: Number, min: 0, max: 5 },
+  lgbtqi_acceptance: { type: Number, min: 0, max: 5 },
+  womens_safety: { type: Number, min: 0, max: 5 },
+  description: String,
+});
 
-const Review = mongoose.model('Review', reviewSchema)
+const Review = mongoose.model("Review", reviewSchema);
 
-module.exports = Review
+module.exports = Review;
