4b65bbe0c79bdd955fc66ee384daa8fd6011395e
Kevin Jacob <kevinjac99@gmail.com>
2022-08-02 15:37:25 +1000

    fixed and added home route. Rendered countries cards in countries route

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,19 @@ import Search from "./components/Search";
 import Carousels from "./components/Carousels";
 import Quotes from "./components/Quotes";
 import Countries from "./components/Countries";
+import Home from "./components/Home";
 import Form from "./components/Form";
 import countryData from "./data/countries-data.json"
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
-      <NavBar />
-      <Search />
-      <Carousels />
-      <Quotes />
-      <hr />
-      <footer className="footer">Contact Us: foobar@gmail.com</footer>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/countries" element={<Countries data={countryData} />} />
       </Routes>
-
 
     </div>
   );
