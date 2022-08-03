ebc13912a491a396307da8e9a3ff7119fb9d2236
Kevin Jacob <kevinjac99@gmail.com>
2022-08-03 16:56:44 +1000

    small changes to service page

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -5,8 +5,8 @@ const Services = () => {
             <div className="service">
                 <img className="serviceImg" src="https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg?w=996&t=st=1659504700~exp=1659505300~hmac=605fcb10e10a1a8d512dd47d00d11c2b90a5cc15208c803125a8dce096e89c4e" alt="people grouping hands together" />
                 <br />
-                <h5>At Safe Journey we understand that discrimination, harassment and racism in any form is intolerable</h5>
-                <p>If you or anyone you know needs to seek help, the following links provide great resources for reaching out</p>
+                <h5>At Safe Journey we believe that discrimination, harassment and racism in any form is intolerable</h5>
+                <p>If you or anyone you know needs to seek help, the following links provide great resources for reaching out:</p>
                 <p>Support for experiences of racism:</p>
                 <a href="https://au.reachout.com/articles/what-to-do-if-youre-experiencing-racism">https://au.reachout.com/articles/what-to-do-if-youre-experiencing-racism</a><br />
                 <a href="https://www.youngminds.org.uk/young-person/coping-with-life/racism-and-mental-health/"> https://www.youngminds.org.uk/young-person/coping-with-life/racism-and-mental-health/</a><br />
@@ -17,7 +17,7 @@ const Services = () => {
                 <a href="https://www.queerspace.org.au/"> https://www.queerspace.org.au/</a><br />
                 <a href="https://www.beyondblue.org.au/who-does-it-affect/lesbian-gay-bi-trans-and-intersex-lgbti-people/helpful-contacts-and-websites"> https://www.beyondblue.org.au/who-does-it-affect/lesbian-gay-bi-trans-and-intersex-lgbti-people/helpful-contacts-and-websites</a><br />
                 <br />
-                <p>Support for experiences of danger:</p>
+                <p>Support for experiences of women's safety:</p>
                 <a href="https://www.wire.org.au/"> https://www.wire.org.au/</a><br />
                 <a href="https://www.vwt.org.au/support-services-2/"> https://www.vwt.org.au/support-services-2/</a><br />
                 <a href="https://www.pmc.gov.au/office-women/economic-security/wess/supporting-women-be-safe-work-and-home"> https://www.pmc.gov.au/office-women/economic-security/wess/supporting-women-be-safe-work-and-home</a>
