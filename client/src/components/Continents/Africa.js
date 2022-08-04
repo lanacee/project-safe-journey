34c3aaba907797499b13a7c5db9609685d3f9713
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-08-04 21:35:36 +1000

    removing useless code

--- client/src/components/Continents/Africa.js
+++ client/src/components/Continents/Africa.js
@@ -1,36 +1,45 @@
-import africaData from "../../data/africa-data.json"
-import { Link } from "react-router-dom"
-import "./Continents.css"
-import Autocomplete from "react-google-autocomplete";
+import africaData from "../../data/africa-data.json";
+import { Link } from "react-router-dom";
+import "./Continents.css";
 
 const AfricaList = (props) => {
-    let min = Math.ceil(400)
-    let max = Math.floor(600)
-    const randomInteger = Math.floor(Math.random() * (max - min) + min)
-    return (
-        <div className="continent_item">
-            <img className="continent_img" src={"https://placeimg.com/" + randomInteger + "/" + randomInteger + "/nature"} alt="random nature" />
-            <Link className="continent_link" to={"/" + props.country.name}><h3 className="continent_link">{props.country.name}</h3></Link>
-        </div>
-    )
-}
-
+  let min = Math.ceil(400);
+  let max = Math.floor(600);
+  const randomInteger = Math.floor(Math.random() * (max - min) + min);
+  return (
+    <div className="continent_item">
+      <img
+        className="continent_img"
+        src={
+          "https://placeimg.com/" +
+          randomInteger +
+          "/" +
+          randomInteger +
+          "/nature"
+        }
+        alt="random nature"
+      />
+      <Link className="continent_link" to={"/" + props.country.name}>
+        <h3 className="continent_link">{props.country.name}</h3>
+      </Link>
+    </div>
+  );
+};
 
 const Africa = () => {
-    const africaList = africaData.map((country) => {
-        return (
-            <AfricaList country={country} key={country._id} />
-        )
-    })
-    return (
-        <div className="all_continents">
-            <h1>Africa</h1>
-            <p className="continent_desc">Africa. There's nowhere like it on the planet for wildlife, wild lands and rich traditions that endure. Prepare to fall in love.</p>
-            <div className="continent_container">
-                {africaList}
-            </div>
-        </div>
-    )
-}
+  const africaList = africaData.map((country) => {
+    return <AfricaList country={country} key={country._id} />;
+  });
+  return (
+    <div className="all_continents">
+      <h1>Africa</h1>
+      <p className="continent_desc">
+        Africa. There's nowhere like it on the planet for wildlife, wild lands
+        and rich traditions that endure. Prepare to fall in love.
+      </p>
+      <div className="continent_container">{africaList}</div>
+    </div>
+  );
+};
 
-export default Africa
+export default Africa;
--- client/src/components/Countries.js
+++ client/src/components/Countries.js
@@ -1,44 +1,42 @@
-import { Link } from 'react-router-dom'
-import "./Countries.css"
-import Autocomplete from "react-google-autocomplete";
+import { Link } from "react-router-dom";
+import "./Countries.css";
 
 const CountryCard = (props) => {
-    let min = Math.ceil(400)
-    let max = Math.floor(600)
-    const randomInteger = Math.floor(Math.random() * (max - min) + min)
+  let min = Math.ceil(400);
+  let max = Math.floor(600);
+  const randomInteger = Math.floor(Math.random() * (max - min) + min);
 
-    return (
-        <div className="countries_item">
-            <img className="countries_img" src={"https://placeimg.com/"+randomInteger+"/"+randomInteger+"/arch"} alt="random architecture" />
-            <Link className="countries_link" to={"/countries/"+props.country.name}><h3 className="countries_link">{props.country.name}</h3></Link>
-        </div>
-    )
-}
-
+  return (
+    <div className="countries_item">
+      <img
+        className="countries_img"
+        src={
+          "https://placeimg.com/" +
+          randomInteger +
+          "/" +
+          randomInteger +
+          "/arch"
+        }
+        alt="random architecture"
+      />
+      <Link className="countries_link" to={"/countries/" + props.country.name}>
+        <h3 className="countries_link">{props.country.name}</h3>
+      </Link>
+    </div>
+  );
+};
 
 const Countries = (props) => {
-    const countryItems = props.data.map((country) => {
-        return (
-            <CountryCard country={country} />
-        )
-    })
-    return (
-        <div >
-            <h1 className="all_countries">All Countries</h1>
-            <div>
-            <Autocomplete
-              apiKey={process.env.REACT_APP_GOOGLE_API}
-              onPlaceSelected={(place) => {
-                console.log(place.formatted_address);
-              }}
-              placeholder="Search for a country"
-            />
-            </div>
-            <div className="countries_container">
-            {countryItems}
-            </div>
-        </div>
-    )
-}
+  const countryItems = props.data.map((country) => {
+    return <CountryCard country={country} />;
+  });
+  return (
+    <div>
+      <h1 className="all_countries">All Countries</h1>
 
-export default Countries
+      <div className="countries_container">{countryItems}</div>
+    </div>
+  );
+};
+
+export default Countries;
