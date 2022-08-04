df6ff85b5f1b671e4b481884db4daf79f9df1ac3
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-08-04 21:54:04 +1000

    can search countries in all countries url

--- client/src/components/Countries.js
+++ client/src/components/Countries.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import "./Countries.css";
+import Select from "react-select";
+import countries from "../data/countries-data.json";
+import { useState } from "react";
 
 const CountryCard = (props) => {
   let min = Math.ceil(400);
@@ -26,15 +29,33 @@ const CountryCard = (props) => {
   );
 };
 
+const options = countries.map((country) => {
+  return { label: country.name, value: country.name };
+});
+
 const Countries = (props) => {
+  const [country, setCountry] = useState(null);
   const countryItems = props.data.map((country) => {
     return <CountryCard country={country} />;
   });
+
+  const handleSelect = ({ value }) => {
+    setCountry({ name: value });
+  };
+
   return (
     <div>
       <h1 className="all_countries">All Countries</h1>
-
-      <div className="countries_container">{countryItems}</div>
+      <Select
+        options={options}
+        name="country"
+        placeholder="Choose your country"
+        isSearchable
+        onChange={handleSelect}
+      />
+      <div className="countries_container">
+        {country ? <CountryCard country={country} /> : countryItems}
+      </div>
     </div>
   );
 };
